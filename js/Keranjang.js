68de3ac2661fba0324d32f02826bca39048f13f4
jeslynchn <celinjeslyn@gmail.com>
2025-07-10 16:42:11 +0700

    Delete js/Keranjang.js

--- js/Keranjang.js
+++
@@ -1,7 +0,0 @@
-let totalKeranjang = 0;
-
-function tambahKeKeranjang() {
-  const jumlah = parseInt(document.getElementById('jumlah').value);
-  totalKeranjang += jumlah;
-  document.getElementById('jumlah-keranjang').textContent = totalKeranjang;
-}
